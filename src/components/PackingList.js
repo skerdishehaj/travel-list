2593c1e1227c07e2c048df8e89a21c12796383f1
Skerdian Shehaj <skerdianshehaj@gmail.com>
2023-09-17 15:39:38 +0200

    Refactor delete item funct

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,16 +1,12 @@
 import Item from "./Item";
 
 export default function PackingList({ items, onDeleteItem }) {
-  const handleDeleteItem = (id) => {
-    console.log("PackageList: " + id);
-    onDeleteItem(id);
-  };
   return (
     <div className="list">
       <ul>
         {items.length > 0 &&
           items.map((item) => (
-            <Item onDelete={handleDeleteItem} key={item.id} item={item} />
+            <Item onDelete={onDeleteItem} key={item.id} item={item} />
           ))}
       </ul>
     </div>
